e26ba696411faf6b3d22555027f2b90e37f28322
Ananth Kumar <ananthkumar2002@gmail.com>
2024-08-05 13:49:16 -0500

    [2] Add endpoint logic

--- app/solvers.py
+++ app/solvers.py
@@ -1,6 +1,14 @@
 from ..server import server
-from flask import jsonify
-
+from flask import jsonify, request
+from ..sudoku import sudoku_solver as sudo_solver
 @server.route('/health', methods=['GET'])
 def head():
     return jsonify("Hello!")
+
+@server.route("/sudoku", methods=["POST"])
+def sudoku():
+    board = request.json["board"]
+    board_dict = sudo_solver.to_dict(board)
+    solved_board = sudo_solver.solve(board_dict)
+    response = {"board": solved_board}
+    return jsonify(response)
