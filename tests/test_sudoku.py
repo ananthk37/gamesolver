29866246e7af95fe7138e3ca3a860073c7959fd1
Ananth Kumar <ananthkumar2002@gmail.com>
2024-08-06 09:39:17 -0500

    [2] finished tests

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -1,5 +1,13 @@
 import pytest
+import json
 
-def test_health(client):
-    response = client.get("/health")
-    assert response.json == "Hello!"
+with open("tests/test_data/sudoku_test.json", "r") as f:
+    data = json.load(f)
+    testdata = [(x["input"],x["solution"]) for x in data["boards"]]
+
+@pytest.mark.parametrize("problem,solution", testdata)
+def test_sudoku_solver(problem, solution, client):
+    response = client.post("/sudoku",json={
+        "board": problem
+    })
+    assert response.json["board"] == solution
