dcb8cc3d245514907fe512a1b8180af6ffd8e9cd
Ananth Kumar <ananthkumar2002@gmail.com>
2024-08-12 13:02:12 -0500

    [4] write endpoint, add tests, write problem conversion for open-source problems

--- app/solvers.py
+++ app/solvers.py
@@ -1,6 +1,7 @@
 from ..server import server
 from flask import jsonify, request
 from ..sudoku import sudoku_solver as sudo_solver
+from ..killer import killer_solver
 @server.route('/health', methods=['GET'])
 def head():
     return jsonify("Hello!")
@@ -10,5 +11,14 @@ def sudoku():
     board = request.json["board"]
     board_dict = sudo_solver.to_dict(board)
     solved_board = sudo_solver.solve(board_dict)
+    response = {"board": solved_board}
+    return jsonify(response)
+
+@server.route("/killer", methods=["POST"])
+def killer():
+    board = request.json["board"]
+    board_dict = sudo_solver.to_dict(board)
+    board_groups = request.json["groups"]
+    solved_board = killer_solver.solve(board_dict, board_groups)
     response = {"board": solved_board}
     return jsonify(response)
---
+++ tests/test_killer.py
@@ -0,0 +1,17 @@
+import pytest
+import json
+
+with open("tests/test_data/killer_test.json", "r") as f:
+    data = json.load(f)
+    testdata = [(x["input_board"], x["input_groups"], x["solution"]) for x in data["boards"]]
+
+
+@pytest.mark.parametrize("board,groups,solution", testdata)
+def test_sudoku_solver(board, groups, solution, client):
+    print(board)
+    print(groups)
+    response = client.post("/killer",json={
+        "board": board,
+        "groups": groups
+    })
+    assert response.json["board"] == solution
