3cf31970e81ed0cf9cdf1786ed2035621d0c06e6
Ananth Kumar <ananthkumar2002@gmail.com>
2024-08-20 16:12:21 -0500

    [14] added kenken endpoint and tests ranging 3x3 -> 5x5

--- app/solvers.py
+++ app/solvers.py
@@ -2,6 +2,8 @@ from ..server import server
 from flask import jsonify, request
 from ..sudoku import sudoku_solver as sudo_solver
 from ..killer import killer_solver
+from ..kenken import kenken_solver
+
 @server.route('/health', methods=['GET'])
 def head():
     return jsonify("Hello!")
@@ -20,5 +22,15 @@ def killer():
     board_dict = sudo_solver.to_dict(board)
     board_groups = request.json["groups"]
     solved_board = killer_solver.solve(board_dict, board_groups)
+    response = {"board": solved_board}
+    return jsonify(response)
+
+@server.route("/kenken", methods=["POST"])
+def kenken():
+    size = request.json["size"]
+    board = request.json["board"]
+    board_dict = sudo_solver.to_dict(board)
+    groups = request.json["groups"]
+    solved_board = kenken_solver.solve(size, board_dict, groups)
     response = {"board": solved_board}
     return jsonify(response)
