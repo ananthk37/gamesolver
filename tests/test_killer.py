bc9e240ec86239d0b99b278526538dcd9c87f007
Ananth Kumar <ananthkumar2002@gmail.com>
2024-08-22 16:10:27 -0500

    [12] add unsolveable test cases to tests

--- tests/test_killer.py
+++ tests/test_killer.py
@@ -4,7 +4,7 @@ import json
 with open("tests/test_data/killer_test.json", "r") as f:
     data = json.load(f)
     testdata = [(x["input_board"], x["input_groups"], x["solution"]) for x in data["boards"]]
-
+    badtests = [(x["input_board"], x["input_groups"]) for x in data["unsolveable"]]
 
 @pytest.mark.parametrize("board,groups,solution", testdata)
 def test_killer_solver(board, groups, solution, client):
@@ -15,3 +15,12 @@ def test_killer_solver(board, groups, solution, client):
         "groups": groups
     })
     assert response.json["board"] == solution
+
+
+@pytest.mark.parametrize("board,groups", badtests)
+def test_unsolveable_kenken(board, groups, client):
+    response = client.post("/killer", json={
+        "board": board,
+        "groups": groups,
+    })
+    assert response.status_code == 400
